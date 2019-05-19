a2f9d8f9c1fa860d7dea65a28c6ccb53a3b3ecfc
Takao Chiba <takao.chiba@gmail.com>
2019-05-19 16:51:26 +0900

    Add simple declaration interface function

--- loco-core/src/main/java/com/sys1yagi/loco/core/Sender.kt
+++ loco-core/src/main/java/com/sys1yagi/loco/core/Sender.kt
@@ -3,6 +3,11 @@ package com.sys1yagi.loco.core
 import com.sys1yagi.loco.core.internal.SmashedLog
 
 interface Sender {
-
     suspend fun send(logs: List<SmashedLog>): SendingResult
+}
+
+fun Sender(send: (List<SmashedLog>) -> SendingResult) = object : Sender {
+    override suspend fun send(logs: List<SmashedLog>): SendingResult {
+        return send.invoke(logs)
+    }
 }
--- loco-core/src/main/java/com/sys1yagi/loco/core/Smasher.kt
+++ loco-core/src/main/java/com/sys1yagi/loco/core/Smasher.kt
@@ -6,3 +6,9 @@ package com.sys1yagi.loco.core
 interface Smasher {
     fun smash(log: LocoLog): String
 }
+
+fun Smasher(smash: (LocoLog) -> String) = object : Smasher {
+    override fun smash(log: LocoLog): String {
+        return smash.invoke(log)
+    }
+}
